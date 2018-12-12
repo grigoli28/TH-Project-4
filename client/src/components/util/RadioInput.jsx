94c454752689b7be86ec1af8abdc9d1bfd97e190
grigol tchkoidze <grigol.tchkoidze@gmail.com>
2018-12-13 00:35:37 +0400

    added category, size and brand filtering options

---
+++ client/src/components/util/RadioInput.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./RadioInput.css";
+
+export default function RadioInput({
+  onChange,
+  name,
+  text,
+  value,
+  currentValue,
+}) {
+  return (
+    <div>
+      <div className="radio">
+        <input
+          id={value}
+          name={name}
+          type="radio"
+          onChange={onChange}
+          value={value}
+          checked={currentValue === value}
+        />
+        <label htmlFor={value} className="radio-label">
+          {text}
+        </label>
+      </div>
+    </div>
+  );
+}
