85a6ff2cb94e14b2312189291643572eb560e96a
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-14 00:07:56 +0400

    login\signup page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import Navigation from "./components/navigation/Navigation";
 import Filters from "./components/filters/Filters";
 import SortBy from "./components/sortBy/SortBy";
 import Footer from "./components/footer/Footer"
+import Login from "./components/login/Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        {/* <Router>
           <Navigation />
         </Router>
         <SortBy />
         <Filters />
         <Footer />
+        <Login /> */}
       </div>
     );
   }
