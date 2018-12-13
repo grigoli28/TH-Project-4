6eb7081786d8446464ced0d7c229b6e89e537d8c
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-13 16:41:58 +0400

    working on contact page

---
+++ client/src/components/contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <div>
+        <div className="Top-image">
+            <h1 className="contact">Contact</h1>
+        </div>
+    </div>
+  );
+}
