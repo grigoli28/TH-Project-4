6393aecb64f664fb35bd1ad87285106628c2a454
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-20 16:21:21 +0400

    useredit

--- client/src/components/adminpage/customerDetails/CustomerDetails.js
+++ client/src/components/adminpage/customerDetails/CustomerDetails.js
@@ -1,31 +1,78 @@
 import React from "react";
 import "./Customer.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-function CustomerDetails({ customer, match }) {
-  const gobackUrl = match.url
-    .split("/")
-    .slice(0, -1)
-    .join("/");
+
+export default class CustomerDetails extends React.Component{
+  state = {
+    email:"ex@gmail.com",
+    date:"02.4.1999",
+    FullName:"so laop"
+  }
+  
+  updateCustomers = () => {
+    const url = "/api/customers";
+    axios
+      .get(url)
+      .then(({ data }) => this.setState({ customers: data }))
+      .catch(err => console.log(err));
+  };componentDidMount() {
+    this.updateCustomers();
+  }
+  render(){
+
+  
+
 
   return (
     <div className="user-details">
-      <Link to={gobackUrl} className="arrow left" />
-      <button className="user-btn">Edit User</button>
+      <Link to="../customers" className="arrow left" />
+      <button className="user-btn" type="submit">Edit User</button>
       <div className="user-img" />
       <div className="user-name">UserName</div>
+      <form className="usernameform">
+          <div className="usergroup userform">
+            <input type="text" required className="userinput" defaultValue={this.state.customers} />
+            <span className="userhighlight" />
+            <span className="userbar" />
+            {/* <label className="userlabel">Email</label> */}
+          </div>
+        </form>
       <div className="user-info">
         <div className="user-personal-info">
           <p className="user-p">Email</p>
-          <span>example@gmail.com</span>
+          <form>
+          <div className="usergroup">
+            <input type="text" required className="userinput" defaultValue={this.state.email}/>
+            <span className="userhighlight" />
+            <span className="userbar" />
+            {/* <label className="userlabel">Email</label> */}
+          </div>
+        </form>
         </div>
+        
         <div className="user-personal-info">
           <p className="user-p">Birth Date</p>
-          <span>02/03/2000</span>
+          <form>
+          <div className="usergroup">
+            <input type="text" required className="userinput" defaultValue={this.state.date}/>
+            <span className="userhighlight" />
+            <span className="userbar" />
+            {/* <label className="userlabel">Email</label> */}
+          </div>
+        </form>
         </div>
         <div className="user-personal-info">
           <p className="user-p">FullName</p>
-          <span>Name Surname</span>
+           <form>
+          <div className="usergroup">
+            <input type="text" required className="userinput" defaultValue={this.state.FullName}/>
+            <span className="userhighlight" />
+            <span className="userbar" />
+            {/* <label className="userlabel">Email</label> */}
+          </div>
+        </form>
         </div>
       </div>
       <div className="user-balance">
@@ -34,6 +81,6 @@ function CustomerDetails({ customer, match }) {
       </div>
     </div>
   );
+  }
 }
 
-export default CustomerDetails;
