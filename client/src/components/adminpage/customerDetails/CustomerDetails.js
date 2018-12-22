51be57246fe896182b4187bffdaa112258e3812e
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-22 17:41:41 +0400

    useredit

--- client/src/components/adminpage/customerDetails/CustomerDetails.js
+++ client/src/components/adminpage/customerDetails/CustomerDetails.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./Customer.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class CustomerDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      User: [],
-    };
-  }
+  state = {
+    user: null
+  };
   componentDidMount() {
-    fetch("/api/Customers")
-      .then(res => res.json())
-      .then(json => {
+    const currentUser = this.props.match.params.prodId;
+    const url = `http://localhost:5000/api/customers/${currentUser}`;
+    axios
+      .get(url)
+      .then(({ data }) => {
         this.setState({
-          User: json
+          user: data
         });
-      });
+      })
+      .catch(err => console.log(err));
   }
   render() {
-    let { User } = this.state;
+    // let { User } = this.state.user;
     return (
       <div className="user-details">
         <Link to="../customers" className="arrow left" />
@@ -34,6 +35,7 @@ export default class CustomerDetails extends React.Component {
               type="text"
               required
               className="userinput"
+              value={this.state.user && this.state.user.username}
             />
             <span className="userhighlight" />
             <span className="userbar" />
@@ -46,15 +48,12 @@ export default class CustomerDetails extends React.Component {
 
             <form>
               <div className="usergroup">
-                {User.map(User => (
-                  <input
-                    key={User.id}
-                    type="text"
-                    required
-                    className="userinput"
-                    defaultValue={User.email}
-                  />
-                ))}
+                <input
+                  type="text"
+                  required
+                  className="userinput"
+                  defaultValue={this.state.user && this.state.user.email}
+                />
 
                 <span className="userhighlight" />
                 <span className="userbar" />
@@ -67,15 +66,12 @@ export default class CustomerDetails extends React.Component {
             <p className="user-p">Birth Date</p>
             <form>
               <div className="usergroup">
-              {User.map(User => (
-                  <input
-                    key={User.id}
-                    type="text"
-                    required
-                    className="userinput"
-                    defaultValue={User.birthdate}
-                  />
-                ))}
+                <input
+                  type="text"
+                  required
+                  className="userinput"
+                  defaultValue={this.state.user && this.state.user.birthdate}
+                />
                 <span className="userhighlight" />
                 <span className="userbar" />
                 {/* <label className="userlabel">Email</label> */}
@@ -86,15 +82,13 @@ export default class CustomerDetails extends React.Component {
             <p className="user-p">FullName</p>
             <form>
               <div className="usergroup">
-              {User.map(User => (
-                  <input
-                    key={User.id}
-                    type="text"
-                    required
-                    className="userinput"
-                    defaultValue={User.name}
-                  />
-                ))}
+                <input
+                  type="text"
+                  required
+                  className="userinput"
+                  defaultValue={this.state.user && this.state.user.name}
+                />
+
                 <span className="userhighlight" />
                 <span className="userbar" />
                 {/* <label className="userlabel">Email</label> */}
@@ -104,7 +98,7 @@ export default class CustomerDetails extends React.Component {
         </div>
         <div className="user-balance">
           <p className="user-p ballance">Ballance</p>
-          <span className="ballance-span">1230$</span>
+          <span className="ballance-span">{this.state.user && this.state.user.balance}</span>
         </div>
         <div className="user-details-cart">
           <ul className="user-productlist">
