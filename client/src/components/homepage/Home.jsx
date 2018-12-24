d2ef6efdc3b161c1c784944b5321161e26bdefaa
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-24 22:22:46 +0400

    home-page

--- client/src/components/homepage/Home.jsx
+++ client/src/components/homepage/Home.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
-export default function Home() {
-  return <img src="/img/girl_in_dark.jpeg" />;
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-page">
+        <div className="home-nav">
+          <p className="home-p">New Collection</p>
+          <div>
+            <Link to="../men" className="home-btn">
+              <p className="link-p">View Collection</p>
+            </Link>
+          </div>
+        </div>
+        <div className="home-mid-nav">
+          <div className="mid-img-1"><p className="home-img-text">Limmited</p></div>
+          <div className="mid-img-2"></div>
+          <div className="mid-img-3"><p className="home-img-text">Edition</p></div>
+        </div>
+        <div className="home-bot-nav">
+          <p className="nike">NIKE</p>
+          <p className="armani">ARMANI</p>
+          <p className="doice">DOICE</p>
+          <p className="adidas">  ADIDAS</p>
+        </div>
+      </div>
+    );
+  }
 }
+export default Home;
