485b7b86941c77e971a76f1fb4f0c58195df1da0
sandro888 <36017398+sandro888@users.noreply.github.com>
2018-12-25 20:19:45 +0400

    react-pose-homePage-responsive

--- client/src/components/homepage/Home.jsx
+++ client/src/components/homepage/Home.jsx
@@ -1,29 +1,97 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import posed from "react-pose";
+import SplitText from "react-pose-text";
 
+const Box = posed.div({
+  hoverable: true,
+  pressable: true,
+  init: {
+    scale: 1,
+    boxShadow: "0px 0px 0px rgba(0,0,0,0)"
+  },
+  hover: {
+    scale: 1.2,
+    boxShadow: "0px 5px 10px rgba(0,0,0,0.2)"
+  }
+});
+const charPoses = {
+  exit: { opacity: 0, y: 20 },
+  enter: {
+    opacity: 1,
+    y: 0,
+    delay: ({ charIndex }) => charIndex * 90
+  }
+};
+
+const Sidebar = posed.div({
+  open: {
+    delay: 100,
+
+    delayChildren: 50,
+    staggerChildren: 50
+  }
+});
+
+const Item = posed.div({
+  open: { y: 0, opacity: 1 },
+  closed: { y: 0, opacity: 0 }
+});
 class Home extends React.Component {
+  state = { isOpen: false };
+
+  componentDidMount() {
+    setTimeout(this.toggle, 1000);
+  }
+
+  toggle = () => this.setState({ isOpen: !this.state.isOpen });
   render() {
+    const { isOpen } = this.state;
+
     return (
       <div className="home-page">
-        <div className="home-nav">
-          <p className="home-p">New Collection</p>
-          <div>
-            <Link to="../men" className="home-btn">
-              <p className="link-p">View Collection</p>
-            </Link>
+        <Sidebar className="sidebar" pose={isOpen ? "open" : "closed"}>
+          {" "}
+          <div className="home-nav">
+            <SplitText
+              initialPose="exit"
+              pose="enter"
+              charPoses={charPoses}
+              className="home-p"
+            >
+              New Collection
+            </SplitText>
+
+            <Item>
+              <div>
+                <Link to="../men" className="home-btn">
+                  <p className="link-p">View Collection</p>
+                </Link>
+              </div>
+            </Item>
           </div>
-        </div>
+        </Sidebar>
         <div className="home-mid-nav">
-          <div className="mid-img-1"><p className="home-img-text">Limmited</p></div>
-          <div className="mid-img-2"></div>
-          <div className="mid-img-3"><p className="home-img-text">Edition</p></div>
+          <Box>
+            <div className="mid-img-1">
+              <p className="home-img-text">Limmited</p>
+            </div>
+          </Box>
+          <Box>
+            <div className="mid-img-2" />
+          </Box>
+          <Box>
+            <div className="mid-img-3">
+              <p className="home-img-text">Edition</p>
+            </div>
+          </Box>
         </div>
         <div className="home-bot-nav">
           <p className="nike">NIKE</p>
           <p className="armani">ARMANI</p>
           <p className="doice">DOICE</p>
-          <p className="adidas">  ADIDAS</p>
+          <p className="adidas"> ADIDAS</p>
         </div>
       </div>
     );
